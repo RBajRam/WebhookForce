1bceedf7d7b48749adaafe028dc501765f358169
Bajo Ramos <r.bajo.ramos@accenture.com>
2018-04-20 09:35:41 +0200

    first commit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/', function(req, res){
 });
 
 app.post('/',function(req, res){
-    var query= req.body.result.resolvedQuery;
+    var query= req.body.result.fulfillment.speech;
     conn.login('r.bajo.ramos@accenture.com','testing1234x7Xg4QsQXWGEfCsC02UeUCSbJ', function(err, res) {
         if (err) { return console.error(err); }
         conn.query(query, function(err, res) {
